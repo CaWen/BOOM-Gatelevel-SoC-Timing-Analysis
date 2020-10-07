96c20118239b539a5a131fbd6259dbff40010b22
CaWen <wenchenan@buaa.edu.cn>
2020-10-07 02:57:25 -0400

    Add files via upload

---
+++ Synthesis/genus.cmd
@@ -0,0 +1,7 @@
+# Cadence Genus(TM) Synthesis Solution, Version 18.14-s037_1, built Mar 27 2019 12:19:21
+
+# Date: Mon Sep 28 22:01:33 2020
+# Host: joker.ece.northwestern.edu (x86_64 w/Linux 4.18.0-193.19.1.el8_2.x86_64) (8cores*8cpus*1physical cpu*Intel(R) Core(TM) i7-9700 CPU @ 3.00GHz 12288KB)
+# OS:   Red Hat Enterprise Linux release 8.2 (Ootpa)
+
+exit
