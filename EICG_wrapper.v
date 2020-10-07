8a0d8048ab3499b9e7492fd38656699de643672e
CaWen <wenchenan@buaa.edu.cn>
2020-10-07 02:57:07 -0400

    Add files via upload

---
+++ EICG_wrapper.v
@@ -0,0 +1,21 @@
+/* verilator lint_off UNOPTFLAT */
+`timescale 1ns/1ps
+
+module EICG_wrapper(
+  output out,
+  input en,
+  input test_en,
+  input in
+);
+
+  reg en_latched /*verilator clock_enable*/;
+
+  always @(*) begin
+     if (!in) begin
+        en_latched = en || test_en;
+     end
+  end
+
+  assign out = en_latched && in;
+
+endmodule
